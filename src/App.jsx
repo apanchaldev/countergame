9b8871590da9a72372d6c102044d45a78c0a75c0
apanchaldev <apanchal.dev@gmail.com>
2024-09-12 20:28:02 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="py-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
+        <span className="h2 pt-4 m-2 text-white-50">Counter Game</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
